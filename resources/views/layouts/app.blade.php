af9e85c395255f7f073229f15bff205d084a698b
Filiz-Kaya <150589723+Filiz-Kaya@users.noreply.github.com>
2023-11-22 17:52:19 +0300

    Add files via upload

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!-- I have deleted this part -->
+        <x-jet-banner />
+
+        <!-- <div class="min-h-screen bg-gray-100">  Original Design Code-->
+        <!-- <div class="min-h-screen"> -->
+            @livewire('navigation-menu')
+
+            <!-- Page Heading -->
+            <!-- @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+             
+                    </div>
+                </header>
+            @endif -->
+
+            <!-- Page Content -->
+            <!-- <main>
+                {{ $slot }}
+            </main> -->
+        <!-- </div> -->
+
+        @stack('modals')
+
+        @livewireScripts
+
